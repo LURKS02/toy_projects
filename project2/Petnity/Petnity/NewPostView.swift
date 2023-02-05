a574c8448087f6478cdb33d8e3df584e70d9f270
LURKS02 <thecrooks2@gmail.com>
2023-02-05 23:23:03 +0900

    test

---
+++ project2/Petnity/Petnity/NewPostView.swift
@@ -0,0 +1,26 @@
+//
+//  NewPostView.swift
+//  Petnity
+//
+//  Created by 디해 on 2023/02/05.
+//
+
+import SwiftUI
+
+struct NewPostData {
+    var postTitle: String
+    var location: String
+    
+}
+
+struct NewPostView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NewPostView_Previews: PreviewProvider {
+    static var previews: some View {
+        NewPostView()
+    }
+}
